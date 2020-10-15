445fd1ba9cf10b6737c4c6c49d21d982a3d387b3
Pratik Dhungel <dhungel.1996@gmail.com>
2020-10-15 18:17:14 +0545

    useState with array of data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import UseStateBasics from './tutorial/1-useState/setup/2-useState-basics';
+import UseStateArray from './tutorial/1-useState/setup/3-useState-array';
 
 function App() {
   return (
     <div className='container'>
-      <UseStateBasics />
+      <UseStateArray />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const clearItem = (id) => {
+    let newPeople = people.filter((person) => person.id != id);
+    setPeople(newPeople);
+  };
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => clearItem(id)}>Remove</button>
+          </div>
+        );
+      })}
+      <button
+        className='btn'
+        onClick={() => {
+          setPeople([]);
+        }}
+      >
+        Clear List
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
