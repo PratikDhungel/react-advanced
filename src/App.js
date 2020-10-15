6e1a85ac53f0a0183d3f902b8ef3c4ecc2d87a78
Pratik Dhungel <dhungel.1996@gmail.com>
2020-10-15 17:32:52 +0545

    useState basic example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import UseStateBasics from './tutorial/1-useState/setup/2-useState-basics';
+
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <UseStateBasics />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('Random Text');
+
+  const handleClick = () => {
+    if (text === 'Random Text') {
+      setText('Hello World');
+    } else {
+      setText('Random Text');
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className='btn' onClick={handleClick}>
+        Click Here!
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
