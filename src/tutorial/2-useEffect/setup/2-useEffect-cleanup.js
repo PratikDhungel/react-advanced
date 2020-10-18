03091748e7bfe708c802a72bf5a733d19b62fa51
Pratik Dhungel <dhungel.1996@gmail.com>
2020-10-18 09:42:50 +0545

    useEffect with cleanUp function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,31 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log('useEffect invoked');
+    window.addEventListener('resize', checkSize);
+    return () => {
+      console.log('Cleanup function returned');
+      window.removeEventListener('resize', checkSize);
+    };
+  });
+  // }, []);
+  // This case of cleanup can also be solved by using an empty dependency list
+  // But it is a better practice to have a clean-up function as later we will working with disappearing components
+
+  console.log('Render');
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{size} PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
