bc785146a0f2541cac5c25598edf020aafff9f8a
Pratik Dhungel <dhungel.1996@gmail.com>
2020-10-18 09:29:23 +0545

    useEffect with single and multiple dependencies

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -4,17 +4,27 @@ import React, { useState, useEffect } from 'react';
 // second parameter
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+  const [secondValue, setSecondValue] = useState(0);
 
   useEffect(() => {
     console.log('useEffect');
     if (value >= 1) {
       document.title = `New Messages ${value}`;
     }
-  });
+  }, [value, secondValue]);
+  // No dependencies list means useEffect is invoked on each render
+  // Empty dependency list means useEffect is invoked on initial render only
+  // Dependency list with value means useEffect is invoked on that value change
+
+  useEffect(() => {
+    console.log('Initial render');
+  }, []);
+
   console.log('Render component');
   return (
     <>
-      <h2>useEffect Basics</h2>
+      <h2>{value}</h2>
+      <h2>{secondValue}</h2>
       <button
         className='btn'
         onClick={() =>
@@ -23,7 +33,17 @@ const UseEffectBasics = () => {
           })
         }
       >
-        Click
+        First Value
+      </button>
+      <button
+        className='btn'
+        onClick={() =>
+          setSecondValue((prevValue) => {
+            return prevValue + 1;
+          })
+        }
+      >
+        Second Value
       </button>
     </>
   );
