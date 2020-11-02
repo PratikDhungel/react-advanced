881e02ae7a307d8fd3310c39fd2ecd809b4c9e8d
Pratik Dhungel <dhungel.1996@gmail.com>
2020-11-02 22:35:14 +0545

    Adding items to list with controlled inputs

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,16 +8,29 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Hello World');
+    if (firstName && email) {
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      // Get previous value of people
+      // Use spread operator to get current values
+      // Return a new array with people and newly added object
+      setPeople((people) => {
+        return [...people, person];
+      });
+      setFirstName('');
+      setEmail('');
+    } else {
+      console.log('Empty values');
+    }
   };
   return (
     <>
       <article>
         <form action='' className='form' onSubmit={handleSubmit}>
           <div className='form-control'>
-            <label htmlFor='firstName'>Name : </label>
+            <label htmlFor='firstName'>First Name : </label>
             <input type='text' id='firstName' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
           </div>
           <div className='form-control'>
@@ -28,6 +41,15 @@ const ControlledInputs = () => {
             Add Person
           </button>
         </form>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className='item' key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </>
   );
